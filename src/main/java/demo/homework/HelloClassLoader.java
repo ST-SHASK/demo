660cee9773a8f3b73bc09fc2404e25def8672cf7
suntao <st_shask@163.com>
2021-09-21 23:44:38 +0800

    JVM 内存结构图 及 各个参数的设置影响范围

--- src/main/java/demo/homework/HelloClassLoader.java
+++ src/main/java/demo/homework/HelloClassLoader.java
@@ -6,6 +6,9 @@ import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 
+/**
+ * 自定义一个 Classloader，加载一个 Hello.xlass 文件，执行 hello 方法，此文件内容是一个 Hello.class 文件所有字节（x=255-x）处理后的文件。
+ */
 public class HelloClassLoader extends ClassLoader{
 
     public static void main(String[] args) throws Exception {
@@ -17,7 +20,7 @@ public class HelloClassLoader extends ClassLoader{
 
     @Override
     public Class<?> findClass(String name) throws ClassNotFoundException {
-        Path path = Paths.get("/Users/suntao/Desktop/demo/src/main/java/demo/homework/Hello.xlass");
+        Path path = Paths.get(System.getProperty("user.dir")+"/src/main/java/demo/homework/Hello.xlass");
         byte[] bytes = new byte[0];
         try {
             bytes = Files.readAllBytes(path);
